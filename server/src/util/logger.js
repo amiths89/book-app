7e8be45c64362fa11cfe68ff98f4f6021570e9d9
Amith <amiths89@gmail.com>
2025-03-24 18:14:02 +0000

    Updated logger path in docker

--- server/src/util/logger.js
+++ server/src/util/logger.js
@@ -2,19 +2,23 @@ import winston from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Local execution path - Uncomment this and comment docker path when executing locally
+//import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+//const logDirectory = path.join(__dirname, '..', '..', '..', 'logs');
 
+//Docker execution path
+const logDirectory = '/app/logs';
 
-const logDirectory = path.join(__dirname, '..', '..', '..', 'logs');
-const logFilePath = path.join(logDirectory, 'bookshelf-%DATE%.log');
-
-if (!fs.existsSync(logDirectory)) {
-    fs.mkdirSync(logDirectory, { recursive: true }); 
+console.log("logDirectory path ",logDirectory);
+try {
+    fs.mkdirSync(logDirectory, { recursive: true });
+} catch (err) {
+    console.error('Error creating log directory:', err);
 }
-
+const logFilePath = path.join(logDirectory, 'bookshelf-%DATE%.log');
 const logFormat = winston.format.combine(
     winston.format.timestamp(),
     winston.format.printf(({ timestamp, level, message, books, ...meta }) => {
